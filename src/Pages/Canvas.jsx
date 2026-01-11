38ee57090c4ca62cbef99130967654545f522344
gino <gino1v@hotmail.com>
2026-01-11 15:30:01 -0300

    vercel changes

--- src/Pages/Canvas.jsx
+++ src/Pages/Canvas.jsx
@@ -12,7 +12,7 @@ const Canvas = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-[rgb(250,219,197)] to-[rgb(255,147,123)] overflow-hidden">
       <Header />
-      <main className="mt-15 md:mt-5 flex-1 flex flex-col items-center gap-4 overflow-visible z-10">
+      <main className="mt-15 md:mt-5 flex-1 flex flex-col items-center gap-4 overflow-visible z-0">
         <MediaPlayer song={song} />
         <DisclaimerModal />
         <div className="w-full flex justify-center">
